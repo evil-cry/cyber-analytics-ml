5e1173b3e0ed84e4a8f89305e78e2dd6e38aace7
evil-cry <dac8484@rit.edu>
2025-02-15 14:23:07 -0500

    Implement DBSCAN training method

--- nids_clustering/src/algorithms.py
+++ nids_clustering/src/algorithms.py
@@ -10,12 +10,13 @@ class _Algorithm():
     def __init__(self, corpus) -> None:
         self.testing_normal_data = _Algorithm.load_corpus(corpus[0])
         self.testing_anomaly_data = _Algorithm.load_corpus(corpus[1])
+
         self.training_normal_data = _Algorithm.load_corpus(corpus[2])
+        self.training_normal_data = _Algorithm.reduce(self.training_normal_data)
 
     @staticmethod
     def load_corpus(path: str) -> np.array:
         data = np.load(path)
-        data = _Algorithm.reduce(data)
 
         return data
 
@@ -95,7 +96,7 @@ class DBSCAN(_Algorithm):
         super(DBSCAN, self).__init__(*args, **kwargs)
 
         self.name = "Density-based spatial clustering of applications with noise"
-        self.e = 0.015 # Estimated from elbow plot
+        self.e = 0.01 # Estimated from elbow plot
         self.min_samples = 4   
 
         if os.path.exists('nids_clustering/cache/distances.npy'):
@@ -104,7 +105,11 @@ class DBSCAN(_Algorithm):
         else:
             self.e_distance_arr = _Algorithm.e_distance(self.training_normal_data)
 
-        graphs.plot_eps(self.e_distance_arr, self.min_samples)
+        # graphs.plot_eps(self.e_distance_arr, self.min_samples)
+        self.clusters = self.train()
+
+        print(f"Clusters identified: {len(self.clusters)}")
+
 
     @staticmethod
     def estimate_eps(data: np.array) -> float:
@@ -112,9 +117,57 @@ class DBSCAN(_Algorithm):
         Estimate the optimal value of epsilon for DBSCAN
         '''
 
-        # distances = _Algorithm.e_distance(data)
+        raise NotImplementedError()    
 
-        # return np.mean(distances)
+    def train(self) -> list[int]:
+        '''
+        Classifies and clusters the core and non-core points of the training data set
+        '''
 
-    def train(self) -> None:
-        pass
+        core_pts = set()
+        non_core_pts = set()
+
+        for i in range(self.training_normal_data.shape[0]):
+            distance = self.e_distance_arr[i]
+
+            n = np.sum(distance < self.e)
+
+            if n <= self.min_samples:
+                non_core_pts.add(i)
+            else: 
+                core_pts.add(i)
+
+        print(f"Core points: {len(core_pts)}")
+        print(f"Non-core points: {len(non_core_pts)}")
+
+        visited = set()
+        clusters = []
+
+        # Cluster core points
+        for core in core_pts:
+            if core not in visited:
+                # Create new cluster
+                cluster = []
+                unchecked_points = [core]
+
+                while unchecked_points:
+                    p = unchecked_points.pop()
+
+                    if p not in visited:
+                        visited.add(p)
+                        cluster.append(p)
+
+                        # Get the neighbors of the current point
+                        distance = self.e_distance_arr[p]
+                        n = np.where(distance < self.e)[0]
+
+                        # Determine if the point is a core point
+                        if p in core_pts:
+                            for neighbor in n:
+                                if neighbor not in visited:
+                                    unchecked_points.append(int(neighbor))
+
+                # Add cluster to list of clusters
+                clusters.append(cluster)
+
+        return clusters
