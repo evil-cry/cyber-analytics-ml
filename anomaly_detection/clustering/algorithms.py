9fe17d3d044a462e460a4f7e05c8e8769caebbbf
IKEA0x90 <ivanchousKutovoy@gmail.com>
2025-02-19 15:55:35 -0500

    merging, fixing evaluations

--- anomaly_detection/clustering/algorithms.py
+++ anomaly_detection/clustering/algorithms.py
@@ -3,6 +3,8 @@ import numpy as np
 import logging
 from sklearn import decomposition, metrics
 from scipy.spatial.distance import cdist
+import utils
+from copy import deepcopy
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -10,87 +12,89 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
 
 class _Algorithm():
-
     def __init__(self, corpus: list, dimensions: int = 2) -> None:
+        self.corpus = deepcopy(corpus) 
         self.dimensions = dimensions
-        self.load_corpus(corpus)
+        self.load_corpus()
 
+        self.normal_count = len(self.testing_normal_reduced)
+        self.attack_count = len(self.testing_attack_reduced)
 
-    def load_corpus(self, corpus: list) -> None:
-        '''
-        Loads and reduces the dimensionality of the data set using PCA
-        '''
+    def load_corpus(self) -> None:
+        for i in range(len(self.corpus)):
+            self.corpus[i] = np.load(self.corpus[i])
 
-        logger.debug("Loading and reducing data")
+        # Assume that the first element in the corpus is the training data.
+        self.training_normal = self.corpus[0]
+        self.testing_normal = self.corpus[1]
+        self.testing_attack = self.corpus[2]
 
         pca = decomposition.PCA(n_components=self.dimensions)
 
-        # Reduce the dimensionality of the data
-        self.training_normal = pca.fit_transform(np.load(corpus[0]))
-        self.testing_normal = pca.transform(np.load(corpus[1]))
-        self.testing_attack = pca.transform(np.load(corpus[2]))
+        # Only fit the training data
+        self.training_normal_reduced = pca.fit_transform(self.training_normal)
+        self.testing_normal_reduced = pca.transform(self.testing_normal)
+        self.testing_attack_reduced = pca.transform(self.testing_attack)
 
-        logger.debug("Training data shape: %s", self.training_normal.shape)
-
-
-    @staticmethod
-    def e_distance(data: np.array) -> np.array:
-        '''
-        Calculate the euclidean distance between all points in the data set
-        '''
-
-        logger.debug("Calculating distances")
-
-        distances = metrics.pairwise.euclidean_distances(data)
-
-        np.save('anomaly_detection/cache/distances.npy', distances)
-
-        return distances
-
-
-    @staticmethod
-    def calculate_metrics(TP: int, FP: int, TN: int, FN: int) -> dict:
+    def calculate_metrics(self, TP: int, FP: int, TN: int, FN: int) -> dict:
         '''
         Calculate metrics based on TP, FP, TN, FN
         '''
 
         try:
             accuracy = (TP + TN) / (TP + FP + TN + FN)
-            tp_rate = TP / (TP + FN)
-            fp_rate = FP / (TN + FP)
+            tpr = TP / (TP + FN)
+            fpr = FP / (TN + FP)
             f1 = (2 * TP) / ((2 * TP) + FP + FN)
 
         except ZeroDivisionError:
             accuracy = 0
-            tp_rate = 0
-            fp_rate = 0
+            tpr = 0
+            fpr = 0
             f1 = 0
 
-        return {
+        self.metrics = {
             'accuracy': accuracy * 100,
-            'tp_rate': tp_rate * 100,
-            'fp_rate': fp_rate * 100,
+            'tpr': tpr * 100,
+            'fpr': fpr * 100,
             'f1': f1 * 100
         }
 
+    def evaluate(self):
+        TP, TN, FP, FN = self.calculate_rates()
+
+        self.calculate_metrics(TP, FP, TN, FN)
+
+        print(self.name)
+        print(f"Accuracy: {self.metrics['accuracy']:.2f}%")
+        print(f"True Positive Rate: {self.metrics['tpr']:.2f}%")
+        print(f"False Positive Rate: {self.metrics['fpr']:.2f}%")
+        print(f"F1 Score: {self.metrics['f1']:.2f}%")
+        print()
+
+    def cluster(self):
+        raise NotImplementedError
+    
+    def calculate_rates(self):
+        raise NotImplementedError
 
 class K_Means(_Algorithm):
-
     def __init__(self, *args, **kwargs) -> None:
         super(K_Means, self).__init__(*args, **kwargs)
 
-        self.name = "K Means Clustering"
+        self.name = "K-Means"
         self.k = 3
         self.tolerance = 1e-4
         self.max_iterations = 100
-        self.threshold = np.percentile(self.training_normal, 95)
-        self.centroids = self.train()
-        self.evalute(self.centroids)
+        self.threshold = np.percentile(self.training_normal_reduced, 95)
+        self.centroids = self.cluster()
+        self.evaluate()
 
 
-    def train(self) -> np.array:
+    def cluster(self) -> np.array:
         '''
         Trains the model
         
@@ -102,30 +106,30 @@ class K_Means(_Algorithm):
 
         logger.debug("--- Identifying Centroids ---")
 
-        indices = np.random.choice(self.training_normal.shape[0], self.k, replace=False)
+        indices = np.random.choice(self.training_normal_reduced.shape[0], self.k, replace=False)
 
         # Initialize centroids
-        centroids = self.training_normal[indices, :]
+        centroids = self.training_normal_reduced[indices, :]
         iterations = 0
 
         for _ in range(self.max_iterations):
             iterations += 1 
             
-            distances = cdist(self.training_normal, centroids, metric='euclidean')
+            distances = cdist(self.training_normal_reduced, centroids, metric='euclidean')
             assignments = np.argmin(distances, axis=1)
 
             new_centroids = np.zeros_like(centroids)
 
             for cluster in range(self.k):
-                cluster_points = self.training_normal[assignments == cluster]
+                cluster_points = self.training_normal_reduced[assignments == cluster]
                 
                 if len(cluster_points) > 0:
                     new_centroids[cluster] = np.mean(cluster_points, axis=0)
 
                 else:
-                    distances = cdist(self.training_normal, centroids, metric='euclidean')
+                    distances = cdist(self.training_normal_reduced, centroids, metric='euclidean')
                     distances_c = np.min(distances, axis=1)
-                    new_centroids[cluster] = self.training_normal[np.argmax(distances_c)]
+                    new_centroids[cluster] = self.training_normal_reduced[np.argmax(distances_c)]
 
             if np.all(np.abs(centroids - new_centroids) < self.tolerance):
                 logger.debug(f"Converged after {iterations} iterations")
@@ -137,25 +141,11 @@ class K_Means(_Algorithm):
             logger.debug(f"Reached maximum iterations: {iterations} without convergance")
 
         return centroids
+    
+    def calculate_rates(self):
+        TP = TN = FP = FN = 0
 
-    def evalute(self, data):
-        '''
-        Evaultes the K-Means Model
-        
-        Steps:
-        1. Iterate through each test sample
-        2. Compute distance to the nearest centroid
-        3. Check distance threshold and see if anomaly
-        4. Count metrics 
-        5. Print total metrics
-        '''
-
-        TP = 0
-        TN = 0
-        FP = 0
-        FN = 0
-
-        for sample in self.testing_normal:
+        for sample in self.testing_normal_reduced:
             distances = np.linalg.norm(sample - self.centroids, axis=1)
             nearest = np.min(distances)
 
@@ -164,7 +154,7 @@ class K_Means(_Algorithm):
             else:
                 TN += 1
 
-        for sample in self.testing_attack:
+        for sample in self.testing_attack_reduced:
             distances = np.linalg.norm(sample - self.centroids, axis=1)
             nearest = np.min(distances)
 
@@ -173,60 +163,51 @@ class K_Means(_Algorithm):
             else:
                 FN += 1
 
-        metrics = _Algorithm.calculate_metrics(TP, FP, TN, FN)
-
-        logger.debug(f"Accuracy: {metrics['accuracy']:.2f}%")
-        logger.debug(f"True Positive Rate: {metrics['tp_rate']:.2f}%")
-        logger.debug(f"False Positive Rate: {metrics['fp_rate']:.2f}%")
-        logger.debug(f"F1 Score: {metrics['f1']:.2f}%")
-
+        return TP, TN, FP, FN
 
 class DBSCAN(_Algorithm):
-
     def __init__(self, *args, **kwargs) -> None:
         super(DBSCAN, self).__init__(*args, **kwargs)
 
-        self.name = "Density-based spatial clustering of applications with noise"
+        self.name = "DBScan"
         self.e = 0.0075 # Estimated from elbow plot
         self.min_samples = 4   
 
-        if os.path.exists('anomaly_detecion/cache/distances.npy'):
-            self.e_distance_arr = np.load('anomaly_detecion/cache/distances.npy')
-
+        if 'p' in kwargs:
+            p = kwargs.get('p')
+            self.distances = utils.Distance(p)
         else:
-            self.e_distance_arr = _Algorithm.e_distance(self.training_normal)
+            self.distances = utils.Distance(False)
+
+        if not self.distances:
+            # Only pass distance if it is given
+            self.distances.calculate(self.training_normal_reduced, **({'d': kwargs['d']} if 'd' in kwargs else {}))
 
         # graphs.plot_eps(self.e_distance_arr, self.min_samples)
-        self.clusters = self.train()
+        self.clusters = self.cluster()
 
-        print(f"Clusters identified: {len(self.clusters)}")
+        logger.debug(f"Clusters identified: {len(self.clusters)}")
 
+        # I don't think we need to separate evaluations
+        '''
         print("--- Clustering normal data ---")
-        self.evaluate(self.testing_normal)
-
-        print("--- Clustering anomaly data ---")
-        self.evaluate(self.testing_attack)
-
-    @staticmethod
-    def estimate_eps(data: np.array) -> float:
+        self.evaluate(self.testing_normal_reduced)
         '''
-        Estimate the optimal value of epsilon for DBSCAN
-        '''
+        
+        self.evaluate()
 
-        raise NotImplementedError()    
-
-    def train(self) -> list[list[int]]:
+    def cluster(self) -> list[list[int]]:
         '''
         Classifies and clusters the core and non-core points of the training data set
         '''
 
-        print("--- Identifying core and non-core points ---")
+        logger.debug("--- Identifying core and non-core points ---")
 
         core_pts = set()
         non_core_pts = set()
 
-        for i in range(self.training_normal.shape[0]):
-            distance = self.e_distance_arr[i]
+        for i in range(self.training_normal_reduced.shape[0]):
+            distance = self.distances[i]
 
             n = np.sum(distance < self.e)
 
@@ -235,10 +216,10 @@ class DBSCAN(_Algorithm):
             else: 
                 core_pts.add(i)
 
-        print(f"Core points: {len(core_pts)}")
-        print(f"Non-core points: {len(non_core_pts)}")
+        logger.debug(f"Core points: {len(core_pts)}")
+        logger.debug(f"Non-core points: {len(non_core_pts)}")
 
-        print(" -- Clustering core points --")
+        logger.debug(" -- Clustering core points --")
 
         visited = set()
         clusters = []
@@ -258,7 +239,7 @@ class DBSCAN(_Algorithm):
                         cluster.append(p)
 
                         # Get the neighbors of the current point
-                        distance = self.e_distance_arr[p]
+                        distance = self.distances[p]
                         n = np.where(distance < self.e)[0]
 
                         # Determine if the point is a core point
@@ -272,50 +253,38 @@ class DBSCAN(_Algorithm):
 
         return clusters
     
-    def evaluate(self, data) -> list[list[int]]:
-        '''
-        Classify the testing data set
-        '''
+    def calculate_rates(self):
+        TP = TN = FP = FN = 0
 
-        TP = 0
-        TN = 0
-        FP = 0
-        FN = 0
+        # Evaluate each dataset separately
+        for data, is_attack in [(self.testing_attack_reduced, True), (self.testing_normal_reduced, False)]:
+            n_samples = data.shape[0]
+            labels = np.full(n_samples, -1)
 
-        n_samples = data.shape[0]
-        labels = np.full(n_samples, -1)
+            for test_i, test_p in enumerate(data):
+                for cluster_i, cluster in enumerate(self.clusters):
+                    for core_i in cluster:
+                        core_p = self.training_normal_reduced[core_i]
+                        distance = np.linalg.norm(test_p - core_p)
 
-        for test_i, test_p in enumerate(data):
-            for cluster_i, cluster in enumerate(self.clusters):
-                for core_i in cluster:
-                    core_p = self.training_normal[core_i]
+                        if distance <= self.e:
+                            labels[test_i] = cluster_i
+                            break
 
-                    distance = np.linalg.norm(test_p - core_p)
-
-                    if distance <= self.e:
-                        labels[test_i] = cluster_i
+                    if labels[test_i] != -1:
                         break
 
-                if labels[test_i] != -1:
-                    break
-        
-        for i, label in enumerate(labels):
-            if i < len(self.testing_attack):
-                if label == -1:
-                    TP += 1  
+            # Evaluate based on the type of data
+            for _, label in enumerate(labels):
+                if is_attack:
+                    if label == -1:
+                        TP += 1  
+                    else:
+                        FN += 1
                 else:
-                    FN += 1  
-            else:
-                if label == -1:
-                    FP += 1 
-                else:
-                    TN += 1          
+                    if label == -1:
+                        FP += 1
+                    else:
+                        TN += 1
 
-        metrics = _Algorithm.calculate_metrics(TP, FP, TN, FN)
-
-        logger.debug(f"Accuracy: {metrics['accuracy']:.2f}%")
-        logger.debug(f"True Positive Rate: {metrics['tp_rate']:.2f}%")
-        logger.debug(f"False Positive Rate: {metrics['fp_rate']:.2f}%")
-        logger.debug(f"F1 Score: {metrics['f1']:.2f}%")
-
-        return labels
+        return TP, TN, FP, FN
